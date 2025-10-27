cd2d468a37ff3ca8c7daca51ee792bb0d14f2a46
Nihalhussainy <nihal.sd007@gmail.com>
2025-10-27 16:02:27 +0530

    cors

--- src/main/java/com/homeride/backend/config/SecurityConfig.java
+++ src/main/java/com/homeride/backend/config/SecurityConfig.java
@@ -112,7 +112,7 @@ public class SecurityConfig {
     @Bean
     CorsConfigurationSource corsConfigurationSource() {
         CorsConfiguration configuration = new CorsConfiguration();
-        configuration.setAllowedOrigins(List.of("http://localhost:5173", "http://localhost:3000", "https_domain_placeholder", "homeride-frontend.vercel.app"));
+        configuration.setAllowedOrigins(List.of("http://localhost:5173","https://homeride-frontend.vercel.app"));
         configuration.setAllowedMethods(Arrays.asList("GET", "POST", "PUT", "DELETE", "OPTIONS", "PATCH"));
         configuration.setAllowedHeaders(Arrays.asList("Authorization", "Cache-Control", "Content-Type"));
         configuration.setAllowCredentials(true);
